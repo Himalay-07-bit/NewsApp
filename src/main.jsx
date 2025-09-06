6e25de150cd982bb38927ea7e19a8c499d9c81f7
Himalay-07-bit <himalaykumar906@gmail.com>
2025-09-06 12:15:51 +0530

    update

--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,10 @@
-import { createRoot } from 'react-dom/client'
-import App from './App'
+import { createRoot } from "react-dom/client";
+import App from "./App";
+import { Analytics } from "@vercel/analytics/react";
 
-createRoot(document.getElementById('root')).render(
-  <App />
-)
+createRoot(document.getElementById("root")).render(
+  <>
+    <App />
+    <Analytics />
+  </>
+);
