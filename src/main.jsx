273ded40218b80a37158ebf4f1184981eaca2f2c
Himalay-07-bit <himalaykumar906@gmail.com>
2025-09-06 12:41:52 +0530

    fix: removed next-specific vercel analytics for vite build

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,8 @@
 import { createRoot } from "react-dom/client";
 import App from "./App";
-import { Analytics } from "@vercel/analytics/react";
 
 createRoot(document.getElementById("root")).render(
   <>
     <App />
-    <Analytics />
-  </>
+    </>
 );
